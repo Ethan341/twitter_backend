a7f001a8318d0b9ad2ce0c10daa5f78aceec09e2
Akash Mule <akashmule341@gmail.com>
2023-06-23 22:53:13 +0530

    connected with mongoDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
-const app = express()
+import {CredentialManager} from '../cred/credentials.js'
+
+
+const app = express();
+const credentials = new CredentialManager();
 
 app.listen(3000,()=>{
     console.log('listening on port 3000 Triggered');
-})
+}) 
+
+
+const client = new MongoClient(credentials.getMongoDBUrl());
+
+async function run() {
+  try {
+    console.log("Started Searching Data in MongoDB....");
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
